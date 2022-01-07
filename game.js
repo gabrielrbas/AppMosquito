eec67e494d283d1833be4f2b34510a2086af8b4d
Gabriel Ribas <gabrielr.7@hotmail.com>
2022-01-07 20:40:47 -0300

    Criando posicoes randomicas

--- game.js
+++ game.js
@@ -1,8 +1,25 @@
-var altura, largura = 0
+var altura = 0
+var largura = 0
 
-function alteraDimensionamento () {
+function alteraDimensionamento() {
     altura = window.innerHeight
-    largura = window.innerWidth
+    largura = window.innerWidth  
+}
 
-    console.log(altura, largura)
+alteraDimensionamento()
+
+function randomizaPosicao() {
+    var posicaoX = Math.floor(Math.random() * largura) - 50
+    var posicaoY = Math.floor(Math.random() * altura)  - 50
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    var mosquito = document.createElement("img")
+    mosquito.src = "imagens/mosquito.png"
+    mosquito.className = "mosquito1"
+    mosquito.style.left = posicaoX + "px"
+    mosquito.style.top = posicaoY + "px"
+    mosquito.style.position = "absolute"
+    document.body.appendChild(mosquito)
 }
