69127eb9c044d7f027b9b9729ff8ef26e9aeb150
Gabriel Ribas <gabrielr.7@hotmail.com>
2022-01-08 16:54:37 -0300

    Criando e removendo mosquitos com tempo

--- game.js
+++ game.js
@@ -9,6 +9,11 @@ function alteraDimensionamento() {
 alteraDimensionamento()
 
 function randomizaPosicao() {
+    
+    if (document.getElementById("mosquito")) {
+        document.getElementById("mosquito").remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura)  - 90
 
@@ -21,6 +26,8 @@ function randomizaPosicao() {
     mosquito.style.left = posicaoX + "px"
     mosquito.style.top = posicaoY + "px"
     mosquito.style.position = "absolute"
+    mosquito.id = "mosquito"
+
     document.body.appendChild(mosquito)
 }
 
